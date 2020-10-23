9ed1d10768b0b05eeb45cd13d353c571d097947e
rebecsan <rebecca.sandelin@gmail.com>
2020-10-23 16:52:49 +0200

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home, Multiplayer, Singleplayer } from './pages';
 
 //Connect to backend url
 const socket = io.connect('/');
@@ -8,8 +10,21 @@ const socket = io.connect('/');
 function App() {
   return (
     <div className="App">
-      <h1>Hangman game</h1>
+      <Router>
+        <Switch>
+          <Route path="/multiplayer">
+            <Multiplayer />
+          </Route>
+          <Route path="/singleplayer">
+            <Singleplayer />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
+
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+// Intermediate module file in order to not have to import each component in a separate import statement
+
+export * from './Home';
+export * from './Multiplayer';
+export * from './Singleplayer';
