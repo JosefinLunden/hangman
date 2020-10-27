573bf57bb78fd9f6e230bfa128e3c2c0af314144
iths-lisa-beverborg <54471572+iths-lisa-beverborg@users.noreply.github.com>
2020-10-27 13:25:26 +0100

    create letters issue 20

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import Letters from "./components/letters/Letters"
 
 //Connect to backend url
 const socket = io.connect('/');
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hangman game</h1>
+      <Letters></Letters>
     </div>
   );
 }
---
+++ src/components/letters/Letters.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Letters.css";
+
+function Letters() {
+    
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", 
+    "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    
+    return(
+        <div className="letter-wrapper" >
+            <div className="letter-container">
+                { alphabet.map(letter => <div className="letter-icons">{letter}</div>) }
+            </div>
+        </div>
+        )     
+    }
+    
+    export default Letters;
