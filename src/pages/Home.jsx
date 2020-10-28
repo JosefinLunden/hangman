186fe05a07da93d2f46e9495defe99f9f409d775
Diana <diana.sofie.nilsson@gmail.com>
2020-10-28 11:50:18 +0100

    Change from class to className

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,10 @@ export const Home = () => (
   // Flex properties with Bootstrap utilities
   <section
     style={{ height: '100vh' }}
-    class="d-flex flex-column justify-content-center align-items-center"
+    className="d-flex flex-column justify-content-center align-items-center"
   >
-    <h1 class=" font-eater text-white">Super Guardians Hangman</h1>
-    <div class="my-5">
+    <h1 className=" font-eater text-white">Super Guardians Hangman</h1>
+    <div className="my-5">
       {/* Buttons only for visualization, change to components later */}
       <Button variant="primary">Start new game</Button>{' '}
       <Button variant="primary">Rules</Button>{' '}
