315890a52ab7561332592f9dd87384913c4e623f
Diana <diana.sofie.nilsson@gmail.com>
2020-10-28 07:51:21 +0100

    Add Letters (component)

--- src/pages/Multiplayer.jsx
+++ src/pages/Multiplayer.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react';
+import Letters from '../components/letters/Letters';
 
 export const Multiplayer = () => (
   <>
     <h1>Multiplayer</h1>
+    <Letters />
   </>
 );
