4dfcb3135f4072571e9c99d922d5069fb06b92f8
Diana <diana.sofie.nilsson@gmail.com>
2020-10-28 16:04:50 +0100

    Add navigation and multigame component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import NewMultiGameButton from '../components/NewMultiGameButton';
 
 export const Home = () => (
   // Flex properties with Bootstrap utilities
@@ -7,11 +8,15 @@ export const Home = () => (
     style={{ height: '100vh' }}
     className="d-flex flex-column justify-content-center align-items-center"
   >
-    <h1 className=" font-eater text-white">Super Guardians Hangman</h1>
+    <h1 className="text-center font-eater text-white">
+      Super Guardians Hangman
+    </h1>
     <div className="my-5">
-      {/* Buttons only for visualization, change to components later */}
-      <Button variant="primary">Start new game</Button>{' '}
-      <Button variant="primary">Rules</Button>{' '}
+      <NewMultiGameButton />{' '}
+      <Button href="/singleplayer" variant="primary">
+        New Singlegame
+      </Button>
     </div>
+    <Button variant="info">Rules</Button>
   </section>
 );
