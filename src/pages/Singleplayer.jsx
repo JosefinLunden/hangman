b7793dc62d970edc424135f7918ef65b2d961632
rebecsan <rebecca.sandelin@gmail.com>
2020-10-29 14:35:01 +0100

    Render div with as many lines as there are letters

--- src/pages/Singleplayer.jsx
+++ src/pages/Singleplayer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import WordDiv from '../components/WordDiv';
 import Letters from '../components/letters/Letters';
 
 export const Singleplayer = () => (
   <>
     <h1>Singleplayer</h1>
+    <WordDiv />
     <Letters />
   </>
 );
