ec6abd840d2a793cb2e2df97276dace263d9cc01
JosefinLunden <josefincslunden@hotmail.se>
2020-10-29 10:29:16 +0100

    #69-disabled-buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Background from '../components/background';
+import '../App.scss';
+
 
 export const Home = () => (
   // Flex properties with Bootstrap utilities
-  <section
-    style={{ height: '100vh' }}
+  <>
+  <Background />
+  <main
+    style={{ height: '100vh'}}
     class="d-flex flex-column justify-content-center align-items-center"
   >
     <h1 class=" font-eater text-white">Super Guardians Hangman</h1>
@@ -13,5 +18,6 @@ export const Home = () => (
       <Button variant="primary">Start new game</Button>{' '}
       <Button variant="primary">Rules</Button>{' '}
     </div>
-  </section>
+  </main>
+  </>
 );
