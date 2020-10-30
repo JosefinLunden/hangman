f2dbaeec923b71aa4bd02b74f9be46c6eb98e437
iths-lisa-beverborg <54471572+iths-lisa-beverborg@users.noreply.github.com>
2020-10-30 10:39:47 +0100

    updates issue13

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,33 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import InfoModal from "../components/infoModal/InfoModal"
+import InfoModal from '../components/infoModal/InfoModal';
 import Background from '../components/Background';
 import NewMultiGameButton from '../components/NewMultiGameButton';
 
 export const Home = () => {
-
   const [modalShow, setModalShow] = useState(false);
-  return(
-  <>
- <Background />
-  <main
-    style={{ height: '100vh'}}
-    class="d-flex flex-column justify-content-center align-items-center">
-    <h1 className="text-center font-eater text-white">
-      Super Guardians Hangman
-    </h1>
-    <div className="my-5">
-      <NewMultiGameButton />{' '}
-      <Button href="/singleplayer" variant="primary">
-        New Singlegame
-      </Button>
-    </div>
-  <Button variant="info"  onClick={() => setModalShow(true)}>Rules</Button>
-  </main>
+  return (
+    <>
+      <Background />
+      <main
+        style={{ height: '100vh' }}
+        class="d-flex flex-column justify-content-center align-items-center"
+      >
+        <h1 className="text-center font-eater text-white">
+          Super Guardians Hangman
+        </h1>
+        <div className="my-5">
+          <NewMultiGameButton />{' '}
+          <Button href="/singleplayer" variant="primary">
+            New Singlegame
+          </Button>
+        </div>
+        <Button variant="info" onClick={() => setModalShow(true)}>
+          Rules
+        </Button>
+      </main>
 
-  
-  <InfoModal
-    show={modalShow}
-    handleClose={() => setModalShow(false)}
-    header={"Game Rules"}
-    body={
-    "The objective of the game Hangman is to guess the word!"}
-  />
+      <InfoModal show={modalShow} handleClose={() => setModalShow(false)} />
     </>
-  )
+  );
 };
-    
