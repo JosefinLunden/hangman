5a324daa25aa74f1f55603eb7d3d55000f21c481
Diana <diana.sofie.nilsson@gmail.com>
2020-10-30 08:11:46 +0100

    Move some functionality to StartNewMultigameModal

--- src/components/NewMultiGameButton.jsx
+++ src/components/NewMultiGameButton.jsx
@@ -1,30 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import Button from 'react-bootstrap/Button';
-const socket = require('../socket').socket;
 
 const NewMultiGameButton = () => {
   const [gameId, setGameId] = useState('');
 
-  const createNewMultiGame = (e) => {
-    e.preventDefault();
-    setGameId(uuidv4());
-  };
-
-  useEffect(() => {
-    if (gameId.length > 0) {
-      // Emit an event to the server to create a new game room
-      socket.emit('createNewGame', gameId);
-    }
-  }, [gameId]);
-
   return (
     <>
-      <Button onClick={createNewMultiGame} variant="primary">
+      <Button onClick={() => setGameId(uuidv4())} variant="primary">
         New Multigame
       </Button>
-      {gameId && <Redirect to={{ pathname: `/multiplayer/${gameId}` }} />}
+      {gameId && (
+        <Redirect
+          to={{
+            pathname: `/multiplayer/${gameId}`,
+            state: 'initGame',
+          }}
+        />
+      )}
     </>
   );
 };
