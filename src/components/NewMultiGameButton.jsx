02fa332e8871f093466c0e4d2a922e226a8ba375
Diana <diana.sofie.nilsson@gmail.com>
2020-11-02 09:20:58 +0100

    Create new game onClick

--- src/components/NewMultiGameButton.jsx
+++ src/components/NewMultiGameButton.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import Button from 'react-bootstrap/Button';
 
+//Enable socket-connection
+const socket = require('../socket').socket;
+
 const NewMultiGameButton = () => {
   const [gameId, setGameId] = useState('');
+
+  //Disable start button if the user has not specified an username
+  useEffect(() => {
+    if (gameId.length > 0) {
+      // Emit an event to the server to create a new game room
+      socket.emit('createNewGame', gameId);
+    }
+  }, [gameId]);
 
   return (
     <>
