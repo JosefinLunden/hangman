71c81ec8e749798d900a75583ff86cc373c0dd75
Diana <diana.sofie.nilsson@gmail.com>
2020-11-02 09:25:11 +0100

    Applied prettier

--- src/components/infoModal/InfoModal.jsx
+++ src/components/infoModal/InfoModal.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React from 'react';
 import './InfoModal.css';
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button } from 'react-bootstrap';
 
 const InfoModal = ({ show, handleClose }) => {
-  return (    
-  <Modal className="modalBg" show={show} onHide={handleClose}>
-      <Modal.Header closeButton>   
+  return (
+    <Modal className="modalBg" show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
         <Modal.Title className="modalTitle">Game Rules</Modal.Title>
       </Modal.Header>
-    <Modal.Body className="modalText"> <p>The objective of the game Hangman is to guess the word!</p></Modal.Body>
-    <Modal.Footer>
-      <Button variant="light" onClick={handleClose}>
-        OK
-      </Button>
-    </Modal.Footer>  
-  </Modal>
+      <Modal.Body className="modalText">
+        {' '}
+        <p>The objective of the game Hangman is to guess the word!</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="light" onClick={handleClose}>
+          OK
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
-}
+};
 
-  export default InfoModal;
+export default InfoModal;
